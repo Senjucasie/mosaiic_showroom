6fdd5ce400302c6b533bb6856630dc4ab89afd1a
francismosaiic <francis.kokkatt@mosaiic.com>
2021-04-27 13:57:10 +0200

    added product buttons appearing alphebetical order,Fixed issue in docker where docker moves after it is placed,added astronaut animation to the project

---
+++ Showroom_1903/Assets/Scripts/ExpandModel.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+
+    /// <summary>
+    /// Placeholder script for exploding the model.
+    /// </summary>
+    public class ExpandModel : MonoBehaviour
+    {
+        // We are using a different model for the expanded view.  Set it here so we can swap it out when we expand.
+        [Tooltip("Game object for the exploded model.")]
+        [SerializeField]
+        private GameObject expandedModel;
+
+        public GameObject ExpandedModel
+        {
+            get { return expandedModel; }
+            set { expandedModel = value; }
+        }
+
+       public bool IsModelExpanded { get; private set; }
+
+         void Awake()
+        {
+          IsModelExpanded = false;
+        }
+
+        public void Expand()
+        {
+            if (IsModelExpanded)
+            {
+                return;
+            }
+
+            IsModelExpanded = true;
+        }
+
+        public void Reset()
+        {
+            IsModelExpanded = false;
+        }
+
+       
+    }
+
