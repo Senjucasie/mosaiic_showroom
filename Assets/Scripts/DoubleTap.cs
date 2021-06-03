7822c9b0dadb0400f71a3a662ed17b055f3797aa
francismosaiic <francis.kokkatt@mosaiic.com>
2021-06-03 13:09:28 +0200

    removed buttons from ui and added pin and unpin functionality

---
+++ Assets/Scripts/DoubleTap.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class DoubleTap : MonoBehaviour, IMixedRealityInputHandler
+{
+    [SerializeField]
+    private float _currentDelay = 0, _maxDelay = 1;
+    [SerializeField]
+    private bool _Timer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_Timer)
+        {
+            _currentDelay += Time.deltaTime;
+            if (_currentDelay >= _maxDelay)
+            {
+                resettimer();
+            }
+        }
+    }
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        // Debug.Log(eventData.MixedRealityInputAction.Description);
+        if (!_Timer)
+        {
+            _Timer = true;
+            Invoke("singletap", 1f);
+        }
+        else
+        {
+            CancelInvoke("singletap");
+            doubletap();
+        }
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+    }
+
+    public void singletap()
+    {
+        
+    }
+    public void doubletap()
+    {
+        
+    }
+    private void resettimer()
+    {
+        _currentDelay = 0;
+        _Timer = false;
+    }
+}
