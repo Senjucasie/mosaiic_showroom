df9b1e4a2e30dd9d87fbc95f91f9f2063c0bea49
francismosaiic <francis.kokkatt@mosaiic.com>
2021-07-05 13:16:02 +0200

    Added the color changing ui according to the discussion on the chair for demo added the prefab for scrolling

---
+++ Assets/Scripts/ColorSlider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Rendering;
+
+public class ColorSlider : MonoBehaviour
+{
+    public Slider slider;
+
+    // Drag & drop handle
+    public Image handle;
+    public GameObject Object;
+
+    public void Start()
+    {
+        slider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+
+    // Invoked when the value of the slider changes.
+    public void ValueChangeCheck()
+    {
+        handle.color = Color.HSVToRGB(slider.value, 1, 1);
+        Object.GetComponent<MaterialInstance>().Material.color = handle.color;
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Chair;
+    public GameObject ChairUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetParent()
+    {
+        ChairUI.transform.parent = Chair.transform;
+    }
+    public void RemoveParent()
+    {
+        ChairUI.transform.parent = null;
+    }
+}
